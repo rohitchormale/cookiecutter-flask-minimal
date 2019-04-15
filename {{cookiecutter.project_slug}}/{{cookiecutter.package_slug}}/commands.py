1839dfe043f94d6aa3cc68bd8b6fe317f7792970
Rohit Chormale <rohitchormale@gmail.com>
2019-04-15 11:40:53 +0530

    Added example forms and commands

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/commands.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/commands.py
@@ -11,3 +11,9 @@ from flask.cli import AppGroup
 {{ cookiecutter.package_slug }}_cli =  AppGroup({{ cookiecutter.package_slug }})
 
 
+@{{ cookiecutter.package_slug }}_cli.command("test-command")
+@click.option("--arg1")
+@click.option("--arg2")
+@click.option("--arg3")
+def test_command(arg1, arg2, arg3):
+    print("Command executed successfully")
--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/forms.py
@@ -5,4 +5,14 @@ author: {{ cookiecutter.author }}
 """
 
 # TODO - create your forms here
+
+# Optionally import flask-wtf and wtforms
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField
+# from wtforms.validators import Length, InputRequired, ValidationError
+
+# class LoginForm(FlaskForm):
+#     username = StringField("Username", validators=[InputRequired(), Length(max=32)])
+#     password = PasswordField("Password", validators=[InputRequired(), Length(min=5, max=32)])
+
 from .models import *
