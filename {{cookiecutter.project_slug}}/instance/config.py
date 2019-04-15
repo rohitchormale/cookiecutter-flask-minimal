aa51f77cd7f985b6a06a9619f4bf0b6d589e66d8
Rohit Chormale <rohitchormale@gmail.com>
2019-04-15 11:54:32 +0530

    Added instance config file and update .gitignore

---
+++ {{cookiecutter.project_slug}}/instance/config.py
@@ -0,0 +1,11 @@
+"""
+This config should not go under version control
+"""
+DEBUG = True
+Testing = True
+
+# Logging
+import logging
+LOG_FILE = "/tmp"
+LOG_SIZE= 1024*1024
+LOG_LEVEL = logging.DEBUG
